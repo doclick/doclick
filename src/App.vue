f3c7ac007f81b1fbbfc613a7c10aee2a69c03b79
Brian Vanegas Parra <bvanegas@fga.com.co>
2019-07-08 17:09:34 -0500

    Initial

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <ion-app>
+      <h1>content</h1>
+      <ion-split-pane>
+        <!--Menu/-->
+        <ion-vue-router main :animated="false" id="menu-content" />
+      </ion-split-pane>
+    </ion-app>
+  </div>
+</template>
+
+<!--style src='./theme.css'></style-->
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Menu from "./components/Menu.vue";
+@Component({
+  components: {
+    Menu
+  }
+})
+export default class App extends Vue {
+    created() {
+      /*this.$store.getters.api.interceptors.response.use((response) => {
+        return response;
+      }, (error) => {
+        if (error.response.status == 401) {
+          //this.$store.dispatch('logout')
+          //this.$router.go('/login');
+        }
+
+        return Promise.reject(error);
+      });
+
+      /*if (Object.keys(this.$store.getters.user).length == 0 && this.$store.getters.user.constructor === Object && this.$store.getters.isLoggedIn) {
+        this.$store.dispatch('user');
+      }*/
+    }
+
+    mounted() {
+    //this.$store.dispatch("loadSessionData");
+    //this.$store.dispatch("loadSpeakerData");
+  }
+}
+</script>
