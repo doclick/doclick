5627e90cc18160051acdf64d64208b3a91d12ad3
Brian Vanegas <brianvanegas@viajesaeromar.com>
2019-08-12 22:14:51 +0000

    separating matches from conversations

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -11,6 +11,19 @@
                     Conversaciones Recientes
                 </ion-list-header>
 
+                <!--ion-item v-for="match in matches" :key="match.id">
+                    <ion-avatar slot="start">
+                        <img :src="match.avatar">
+                    </ion-avatar>
+                    <ion-label>
+                        <h2>{{match.name}} {{match.last_name}}</h2>
+                    </ion-label>
+                </ion-item-->
+
+                <ion-list-header>
+                    Matches
+                </ion-list-header>
+
                 <ion-item v-for="match in matches" :key="match.id">
                     <ion-avatar slot="start">
                         <img :src="match.avatar">
