50377f0e5c0e6732275e214da191e7790190e30b
Brian Vanegas <brianvanegas@viajesaeromar.com>
2019-08-12 20:06:34 -0500

    Discover view using card

--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Discover</h1>
+    <ion-content>
+        <ion-header padding>
+            <ion-title>Discover</ion-title>
+        </ion-header>
+        <ion-card>
+            <ion-card-header>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+            <ion-card-title>Card Title</ion-card-title>
+            </ion-card-header>
+
+            <ion-card-content>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+            </ion-card-content>
+        </ion-card>
+    </ion-content>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { mapGetters } from 'vuex'
+@Component({
+  components: {
+
+  },
+  computed: {
+    //...mapGetters({'persons': 'persons/persons'}),
+  },
+})
+export default class Discover extends Vue {
+  
+  public mounted() {
+      console.log(this.$route.params)
+  }
+}
