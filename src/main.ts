2b66ce8c0ca81711b7896059c0de46913fff968f
Brian Vanegas <brianvanegas@viajesaeromar.com>
2019-08-13 21:02:03 +0000

    Socket IO Settings

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,25 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
+
+import IonicVue from '@ionic/vue';
 import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
 
-import IonicVue from '@ionic/vue';
+Vue.use(IonicVue);
 
-Vue.use(IonicVue); 
+import VueSocketIO from 'vue-socket.io';
+import io from 'socket.io-client';
+const SocketInstance = io.connect('http://192.168.10.10:3000');
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketInstance,
+  vuex: {
+    store,
+    mutationPrefix: 'SOCKET_'
+  },
+}));
+
 
 Vue.config.productionTip = false;
 
