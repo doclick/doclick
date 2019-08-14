2431fc1c2eb737e4604144612f27decb4ab7dec9
Brian Vanegas <brianvanegas@viajesaeromar.com>
2019-08-13 23:03:28 -0500

    Chat component

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -29,7 +29,7 @@
                         <img :src="match.avatar">
                     </ion-avatar>
                     <ion-label>
-                        <h2>{{match.name}} {{match.last_name}}</h2>
+                        <h2 @click="loadChat(match.id)">{{match.name}} {{match.last_name}}</h2>
                     </ion-label>
                     <ion-badge col-2 color="danger" v-if="!match.pivot.view_at">Nuevo</ion-badge>
                 </ion-item>
@@ -39,10 +39,11 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Chat from './Chat'
 import { mapGetters } from 'vuex'
 @Component({
   components: {
-
+      Chat
   },
   computed: {
       ...mapGetters({'matches': 'persons/matches'}),
@@ -50,6 +51,23 @@ import { mapGetters } from 'vuex'
 })
 export default class Matches extends Vue {
     public matches!: any[];
+
+    public loadChat(user_id_to) {
+        console.log("load chat modal")
+        return this.$ionic.modalController
+        .create({
+          component: Chat,
+          componentProps: {
+            data: {
+              content: 'New Content',
+            },
+            propsData: {
+              user_id_to: user_id_to
+            },
+          },
+        })
+        .then(m => m.present())
+    }
 
     public mounted() {
         this.$store.dispatch('persons/matches');
